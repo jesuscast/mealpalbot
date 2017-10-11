7356082eb16edabf9de0f3a47ea71b3e9388fcc5
Andres <andres@unify.id>
2017-10-10 18:00:43 -0700

    Fixup

--- order_lunch.py
+++ order_lunch.py
@@ -3,10 +3,6 @@ from selenium.common.exceptions import NoSuchElementException
 import os
 import time
 
-driver = webdriver.Chrome()
-
-#driver.quit()
-
 class DriverWrapper:
     html_tags = ["a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "main", "map", "mark", "menu", "menuitem", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr"]
     def get_element(self, selector, father = None):
@@ -85,6 +81,7 @@ class MealPal(DriverWrapper):
         self.driver.quit()
 
 if __name__=="__main__":
+    driver = webdriver.Chrome()
     username = os.getenv("username")
     password = os.getenv("password")
     search = os.getenv("search")
